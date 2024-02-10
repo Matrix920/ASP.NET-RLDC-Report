f7b4c9214c2405521234e692aa86ded50f3b6dba
matrix920 <matrix.leader.920@gmail.com>
2024-02-10 10:43:54 +0400

    Add project files.

---
+++ RLDC_Report/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RLDC_Report._Default" %>
+
+<%@ Register TagPrefix="rsweb" Namespace="Microsoft.Reporting.WebForms" Assembly="Microsoft.ReportViewer.WebForms" %>
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+  <div style="padding: 15px">
+      <div align="center">
+          <asp:Label runat="server" Text="Select Department" ID="ctl02" ForeColor="Black"></asp:Label>
+          
+          
+          <asp:DropDownList ID="DropDownDepartment" runat="server" DataTextField="Name" DataValueField="Id" DataSourceID="SqlDataSource1"></asp:DropDownList><asp:SqlDataSource runat="server" ID="SqlDataSource1" ConnectionString="<%$ ConnectionStrings:DefaultConnection %>" SelectCommand="SELECT [Id], [Name] FROM [Departments]"></asp:SqlDataSource>
+          <asp:Button ID="Button1" runat="server" Text="Load Report" ForeColor="White" BackColor="#660066" OnClick="Button1_Click" />
+
+      </div>
+      <div align="center">
+          <rsweb:ReportViewer ID="ReportViewer" runat="server" Width="700px"></rsweb:ReportViewer>
+          &nbsp;</div>
+  </div>
+
+</asp:Content>
---
+++ RLDC_Report/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RLDC_Report
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString.ToString();
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            string query = @"SELECT  Departments.Id AS IdDepartmentId, Departments.Name AS DepartmentName, Employees.DepartmentId, Employees.Name, Employees.Address, Employees.MobileNumber
+                            FROM   Departments INNER JOIN
+                                    Employees ON Departments.Id = Employees.DepartmentId
+                            WHERE  (Employees.DepartmentId = @Param1)";
+            string departmentsQuery = @"SELECT Id, Name FROM Departments";
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+            sqlCommand.Parameters.Add(new SqlParameter("@Param1", DropDownDepartment.SelectedValue.ToString()));
+            SqlDataAdapter s = new SqlDataAdapter(sqlCommand);
+            DataTable dt = new DataTable();
+            s.Fill(dt);
+            //ReportParameter parameter = new ReportParameter("ReportParameter1",);
+            ReportViewer.LocalReport.DataSources.Clear();
+            ReportDataSource rds = new ReportDataSource("DataSet1", dt);
+            ReportViewer.LocalReport.ReportPath = Server.MapPath("EmployeeReport.rdlc");
+            ReportViewer.LocalReport.DataSources.Add(rds);
+            ReportViewer.LocalReport.Refresh();
+        }
+    }
+}
